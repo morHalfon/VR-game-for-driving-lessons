7665f5244bf9614cdcf486a894f4e8e346c8d8fc
mor halfon <mor.halfon155@gmail.com>
2019-01-22 18:47:45 +0200

    the city we will use in the alpha version

---
+++ city/temp (1)/Assets/scripts/ControlCar.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlCar : MonoBehaviour {
+
+    public EditorPath pathToFollow;
+    public int currWayPointID = 0;
+    public float speed;
+    private float reachDistance = 1.0f;
+    public float rotationSpeed = 5.0f;
+    public string pathName;
+
+    Vector3 lastPos;
+    Vector3 currPos;
+
+	// Use this for initialization
+	void Start ()
+    {
+        pathToFollow = GameObject.Find(pathName).GetComponent<EditorPath>();
+        lastPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float distance = Vector3.Distance(pathToFollow.path_objs[currWayPointID].position, transform.position);
+        Vector3 target = pathToFollow.path_objs[currWayPointID].position;
+        transform.position = Vector3.MoveTowards(transform.position, target, Time.deltaTime * speed);
+        var rotation = Quaternion.LookRotation(target - transform.position);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * rotationSpeed);
+
+        if(distance <= reachDistance)
+        {
+            currWayPointID++;
+        }
+	}
+}
---
+++ city/temp (1)/Assets/scripts/EditorPath.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EditorPath : MonoBehaviour {
+
+    public Color lineColor = Color.blue;
+    public List<Transform> path_objs = new List<Transform>();
+    Transform[] transforms;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = lineColor;
+        transforms = GetComponentsInChildren<Transform>();
+        path_objs.Clear();
+        foreach(Transform path_obj in transforms)
+        {
+            if(path_obj != this.transform)
+            {
+                path_objs.Add(path_obj);
+            }
+        }
+        int currNode;
+        Vector3 currentNode;
+        Vector3 previousNode;
+        Gizmos.DrawSphere(path_objs[0].position, 0.3f);
+        for (currNode = 0; currNode < path_objs.Count; currNode++)
+        {
+            currentNode = path_objs[currNode].position;
+            if (currNode > 0)
+            {
+                previousNode = path_objs[currNode - 1].position;
+                Gizmos.DrawLine(previousNode, currentNode);
+                Gizmos.DrawSphere(currentNode, 0.3f);
+            }
+        }
+    }
+}
