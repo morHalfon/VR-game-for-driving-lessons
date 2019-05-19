5ef485bc7f1afd98fa91b990f59f30898545c8a5
mor halfon <mor.halfon155@gmail.com>
2019-05-19 19:41:44 +0300

    change line in ControlCar

--- DrivingLessons/Assets/scripts/ControlCar.cs
+++ DrivingLessons/Assets/scripts/ControlCar.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ed011c53558b83dc8dc63e44ee2730748ae7254d4d2345275495e1794c959d42
-size 6133
+oid sha256:b02b8082fcdaa388fabcc90b22bb3bf8be10e2f81f24e985ec2f12b132eaaf08
+size 6137
