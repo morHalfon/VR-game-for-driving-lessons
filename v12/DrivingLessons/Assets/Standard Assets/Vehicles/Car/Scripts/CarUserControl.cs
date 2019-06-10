3d17c5214c4b14236ed0b575e4e61bd3b99e1abb
hallel1 <halleli100@gmail.com>
2019-06-10 16:44:07 +0300

    v12 restart

---
+++ v12/DrivingLessons/Assets/Standard Assets/Vehicles/Car/Scripts/CarUserControl.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:38acf2a99195acedc6d6140abd537507f464d3c307511fc3c9ff9d80110a0aaf
+size 1565
---
+++ v12/DrivingLessons/Assets/scripts/EditorPath.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0d801c98aedceee5ff37f120ba759473c8a4cb0dabd6f95a00c6a1dcd29e5654
+size 1161
